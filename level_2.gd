9329308c83a930a9541caaaee023b47a240f859f
andrebins16 <a.bins@edu.pucrs.br>
2023-10-30 17:53:22 -0300

    v4

--- level_2.gd
+++ level_2.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 const lines:Array[String] = ["Now you have to race against time... Run!"]
 func _ready():
-	GameManager.start_dialog(Vector2(200,0),lines)
+	GameManager.start_dialog(Vector2(200,400),lines)
